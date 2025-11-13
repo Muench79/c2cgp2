44dab5d2bf81d12f36ea76b099bc284ab9c93d30
joemapy <joe.ma@web.de>
2025-11-13 16:31:55 +0100

    Test

--- PP1/test.py
+++ PP1/test.py
@@ -1,8 +1,16 @@
 import re
 
 def test(m):
-    print(m)
-    return m
+    print('hhhh',m["count"])
+    x = int(m["count"])+1
+    return str(x)
+#lambda m: str(int(m.group()) + 1)
 
-r = re.sub(r'.*', test, "c:\\tets\test_0.csv")
+xx = 'c:\\tets\\test_0.csv'
+while xx != "c:\\tets\\test_999.csv":
+    r = re.sub(r'(?<=\_)(?P<count>[0-9]{1,3})(?=\.csv|\.json)', lambda m: str(int(m.group('count')) + 1) if int(m.group('count')) < 999 else m.group('count'), xx)
+    xx = r
+    print(r)                                                       #lambda m: str(int(m.group('count')) + 1) if int(m.group('count')) < 999 else m.group('count')
+
+#r = re.sub(r'(?<=\_)(?P<count>[0-9]{1,3})(?=\.csv|\.json)', test, "c:\\tets\\test_0.csv")
 print(r)
