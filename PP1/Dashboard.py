a058eda86f2aff3b25e15182fd1b0182aa7ef936
Matschenz <VWMPDNI@vw.vwgroup.com>
2025-11-17 16:11:50 +0100

    Update Design + Dash Stufe 3 hinzugefügt

---
+++ PP1/Dashboard.py
@@ -0,0 +1,143 @@
+import pandas as pd
+from dash import Dash, html, dcc, Input, Output, dash_table
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output, State
+import plotly.express as px
+import numpy as np
+
+Dashdf = pd.read_csv("data_storage.csv")
+
+#Optional timestampspalte in Zeitwerte konvertieren
+Dashdf["timestamp"] = pd.to_datetime(Dashdf["timestamp"], unit="s")
+Dashdf["fahrzeit_s"] = (Dashdf["timestamp"] - Dashdf["timestamp"].iloc[0]).dt.total_seconds()
+
+
+max_speed=Dashdf["speed"].max()
+min_speed=Dashdf["speed"].min()
+avg_speed=Dashdf["speed"].mean()
+
+#Daten für Trapezmethode vorbereiten
+s = Dashdf.sort_values("timestamp").reset_index(drop=True)
+t = (s["timestamp"] - s["timestamp"].iloc[0]).dt.total_seconds().to_numpy()  # (Normieren auf Startzeit) iloc[0] bezeichnet ersten Wert der Spalte / .dt.total_seconds() wandelt eine Timedelta-Spalte in „Sekunden seit Start“ als Zahlenformat um
+v = (s["speed"] * (1000/3600.0)).to_numpy()                    #Umwandlung in m/s  (.to_numpy() wandelt Daten in numpy-Array um (ohne Spaltenname, ohne Index))
+
+# Trapezintegration
+total_dist = np.trapezoid(v, t)                                         # Integration mit Trapezmethode
+#print(total_dist, type(total_dist))                                     # zum Check
+print(f"Zurückgelegte Strecke: {total_dist:.2f} m")
+
+s2 = Dashdf["timestamp"].dropna().sort_values()                      # Werte sortieren
+total_time=s2.iloc[-1] - s2.iloc[0]
+total_time2=s.iloc[-1] - s.iloc[0]
+print(f"maxspeed: {max_speed}; minspeed: {min_speed}; avgspeed: {avg_speed}; totaldist: {total_dist}; totaltime: {total_time}; totaltime2: {total_time2}")
+
+
+# Formatierung
+def fmt(v, unit=""):
+    return f"{v:.2f}{(' ' + unit) if unit else ''}"
+
+def fmt_time(sec):
+    sec = float(sec)
+    m, s = divmod(int(round(sec)), 60)
+    h, m = divmod(m, 60)
+    return f"{h:02d}:{m:02d}:{s:02d}"
+time_delta = Dashdf["timestamp"].max() - Dashdf["timestamp"].min()
+total_time_sec = time_delta.total_seconds()
+print (total_time_sec)
+print(fmt_time(total_time_sec))  # z.B. 00:00:20
+
+
+    # ----Dash App erzuegen----
+app = Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
+
+card_style = {                                              # Stil der Karten vorgeben
+    "border": "1px solid #ddd",
+    "borderRadius": "6px",
+    "padding": "6px 8px",
+    "minWidth": "80px",
+    "boxShadow": "0 2px 8px rgba(0,0,0,0.06)",
+    "background": "#e6f7e6"                     #Pastellgrün: #e6f7e6, hellgrau:#e6e6e6
+}
+
+fig = px.line(Dashdf,x="timestamp",y="speed",title="<b><i>Fahrgeschwindigkeit</i></b>",labels={"timestamp": "Zeit", "speed": "Speed (m/s)"})                 # Graph vordefinieren, <b>...</b> fett geschrieben; <i>…</i> --> kursiv
+fig.update_layout(title={   "x": 0.5,               # Titel ausrichten  0 = links, 0.5 = Mitte, 1 = rechts
+                            "xanchor": "center"},
+                  title_font=dict(size=24,           # größer
+                                # family="Arial",  # optional Schriftart
+                                # color="black"    # optional Farbe
+                                ),
+                )
+
+app.layout = html.Div([                                                         # zentriert       #Schriftgröße     #schriftart kursiv      Zeilenabstand oben/unten
+                    html.Div([dbc.Row([html.H3("Fahrdaten – KPIs", id="h-1", style={"textAlign": "center", "fontSize": "42px", "fontStyle": "italic", "margin": "16px 0"})]),
+                        dbc.Row([
+                                dbc.Col(html.Div([
+                                    html.Div("Min Speed [m/s]",style={"color": "#666", "fontSize": "14px"},),
+                                    html.H2(fmt(min_speed, "m/s"),style={"margin": "6px 0 0"},),], style=card_style,)),
+                                dbc.Col(html.Div([
+                                    html.Div("Max Speed [m/s]",style={"color": "#666", "fontSize": "14px"},),
+                                    html.H2(fmt(max_speed, "m/s"),style={"margin": "6px 0 0"},),],style=card_style,)),
+                                dbc.Col(html.Div([
+                                    html.Div("Mean Speed [m/s]",style={"color": "#666", "fontSize": "14px"},),
+                                    html.H2(fmt(avg_speed, "m/s"),style={"margin": "6px 0 0"},),],style=card_style,)),
+                                dbc.Col(html.Div([
+                                    html.Div("„zurückgelegte Strecke“ [m]",style={"color": "#666", "fontSize": "14px"},),
+                                    html.H2(fmt(total_dist, "m"),style={"margin": "6px 0 0"},),],style=card_style,)),
+                                dbc.Col(html.Div([
+                                    html.Div("„gesamte Fahrzet [s]“ ",style={"color": "#666", "fontSize": "14px"},),
+                                    html.H2(fmt_time(total_time_sec),style={"margin": "6px 0 0"},),],style=card_style,)),
+                                ]),
+                            ],
+                        style={"display": "flex",               #Div als Flex Container definieren --> alle Kinder (also dbc.Row un Col) werden von Flexbox angeordnet
+                                "gap": "16px",                  # Abstand zwischen den einzelnen boxen
+                                "flexWrap": "wrap",             # Wenn nicht genug Platz in der Hauptrichtung ist, dürfen Elemente in die nächste Zeile/Spalte umbrechen
+                                "alignItems": "stretch",        # Bestimmt die Ausrichtung quer zur Hauptrichtung, "row" → vertikal, "column" → horizontal,"stretch" → Kinder werden so weit wie möglich „aufgezogen“
+                                "flexDirection": "column",      #Hauptrichtung der Anordnung der Boxen (hier Spaltenausrichtung)
+                                }
+                            ),
+                    html.Br(),                             # etwas Abstand
+                     
+    
+                    html.Div([
+                        html.Div(                          # Dropdown oben rechts über dem Diagramm
+                            dcc.Dropdown(
+                                id="drpd-1",options=[  {"label": "Geschwindigkeit [m/s]", "value": "speed"},                    # label definiert den Anzeigenamen im Dropdown
+                                                        {"label": "Lenkwinkel [Grad]", "value": "steering_angle"},              # Value definiert den Wert, der übergeben wird an die Callbackfunktion
+                                                        {"label": "Fahrtrichtung [-]", "value": "direction"},
+                                                        {"label": "Abstand z. Hinderniss [-]", "value": "ultrasonic"}, 
+                                                            ],
+                                value="speed",                                          #Startwert bzw defaultwert
+                                clearable=False,
+                                style={"width": "250px"}),                              #Breite definieren
+                            style={"display": "flex",
+                                   "justifyContent": "flex-end",  # ➜ nach rechts schieben
+                                    "marginBottom": "8px"},
+                            ),
+                        dcc.Graph(id="gr-1",figure=fig)
+                        ])
+                    ])
+
+@app.callback(		                                                    #Callbackfunktion Dropdown
+            Output("gr-1","figure"),                                    # Figure soll geändert werden		
+            Input("drpd-1", "value"),                                    # Wert von Callback erfragen
+            State("drpd-1", "options")                                  # zusätzlich dir Options aus Zeile 85 bis 88 übergeben                               
+    )
+
+
+def update_graph(sel_value, options):                               # mit dieser Zeile wird Wert von Dropdown an sel_value übergeben
+    label = sel_value                                               # Fallback wenn if Schleife nichts findet
+    for opt in options:                                             # Schleife, um die Options durchzugehen opt wäre in der Matlabwelt options(i)
+        if opt["value"]==sel_value:                                 # wenn opt und sel_value übereinstimmen 
+            label=opt["label"]                                      # dann label aus opt rausschreiben
+            break                                                   # for-Schleife abbrechen
+
+    fig = px.line(Dashdf,x="timestamp",y=sel_value,title=f"<b><i>{label}</i></b>",labels={"timestamp": "Zeit", sel_value: label},)   # plot definieren. <b>...</b> fett geschrieben; <i>…</i> --> kursiv
+    fig.update_layout(title_x=0.5,title_xanchor="center",title_font=dict(size=24))      # titel zentrieren und figure updaten
+    print(f"options sind: {options}")
+    return fig
+
+if __name__ == "__main__":
+ 	
+    app.run(host="0.0.0.0", port=8053, debug=True)#debug=True setzt das Programm in debug-mode und ich muss nicht andauernd das Programm abbrechen und neu starten wenn ich was geändert habe, sondern es wird dauerhaft aktualisiert
+
+
